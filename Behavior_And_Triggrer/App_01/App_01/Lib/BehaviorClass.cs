ce9971824f49b1efee8e8c4cbcbf5f542415cbe3
joaojuvencio87 <joaojuvencio87@gmail.com>
2020-10-05 15:25:21 -0300

    Commit 1

---
+++ Behavior_And_Triggrer/App_01/App_01/Lib/BehaviorClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App_01.Lib
+{
+    public class BehaviorClass : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += MaiusculoAction;
+        }
+
+        private void MaiusculoAction(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                ((Entry)sender).Text = e.NewTextValue.ToUpper();
+
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= MaiusculoAction;
+        }
+    }
+}
---
+++ Behavior_And_Triggrer/App_01/App_01/Lib/EmailValidarTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App_01.Lib
+{
+    public class EmailValidarTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (sender.Text.IndexOf("@") <= 0)
+            {
+                sender.TextColor = Color.Red;
+            }
+            else
+            {
+                sender.TextColor = Color.Blue;
+            }
+
+        }
+    }
+}
